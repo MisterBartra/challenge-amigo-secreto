f07d2809d814ec8273b37eb5b9ce65ddaaeee7c6
MisterBartra <jedubartra21@gmail.com>
2025-02-13 11:34:37 -0500

    Mejoranmdo y generalizando usos prácticos

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigoElement = getElementById('amigo')
-let amigosElement = getElementById('listaAmigos'), amigos = [];
-let resultado = getElementById('resultado');
+let amigoElement = document.getElementById('amigo')
+let amigosElement = document.getElementById('listaAmigos'), amigos = [];
+let resultado = document.getElementById('resultado');
 
 
-function asignarTextoElemento(HTMLelemento, texto) {
-    let elementoHTML = document.querySelector(HTMLelemento);
-    elementoHTML.innerHTML = texto;
-    return;
+function encontrarEnAmigos(mensaje="", idLista = 0) {
+    for (var i=idLista; i < amigos.length; i++) { // Comprobar si el valor ya existe en la lista.
+        if (amigoElement.value !== amigos[i]) continue;
+        else {alert(mensaje); return true;} // Caso: Coinciden palabras
+    }
+    return false; // Caso: No encontrado
 }
 
 function agregarAmigo() {
-    if (amigoElement.innerHTML !== '') {
-        amigoLIActual = document.createElement('li');
-        amigos.push(amigoElement.innerHTML);
-        amigoLIActual.innerHTML = amigoElement.innerHTML; // Momento dónde el dato se cede al nuevo elemento li
-        amigosElement.appendChild(amigoLIActual); // Se agrega el elemento <li> en la carpeta
-        amigoElement.innerHTML = ''; // Se limpia el input
+    if (amigoElement.value !== '' && !encontrarEnAmigos("Existe un amigo con este nombre.")) { // Se descarta el vacío
+        amigoLIActual = document.createElement('li'); // Se prepara el elemento
+        amigos.push(amigoElement.value); // Enlisto el valor dentro de amigos
+        amigoLIActual.innerHTML = amigoElement.value; // Momento dónde se entrega el dato al nuevo elemento <li>
+        amigosElement.appendChild(amigoLIActual); // Se agrega el elemento <li> en el elemento <ul>
+        amigoElement.value = ''; // Se limpia el input
     } else {
-        alert("Por favor, inserte un nombre.");
+        alert(`Por favor, ingrese ${amigos[0] == undefined ? "un" : "otro"} nombre de amigo.`);
     }
 }
 
 function sortearAmigo() {
     if (amigos.length === 0) return alert("La lista de amigos está vacío.");
     let posicionAmigoDeLaLista = Math.floor(Math.random()*amigos.length);
-	amigoEncontradoEnLista = false;
-    for (let idLista=0; !amigoEncontradoEnLista; idLista++) {
-		amigoEncontradoEnLista = amigoElement.innerHTML === amigos[idLista];
-	}
-	if (!amigoEncontradoEnLista) alert(`La persona elegida fue ${amigos[posicionAmigoDeLaLista]}`)
-	else alert("Adivinaste!\n" + `${amigoElement.innerHTML} fue la persona elegida.`) 
+	amigoEncontradoEnLista = encontrarEnAmigos("Exacto!", posicionAmigoDeLaLista);
+    if (!amigoEncontradoEnLista) alert(`La persona elegida fue ${amigos[posicionAmigoDeLaLista]}`)
+	else alert("Adivinaste!\n" + `${amigoElement.value} fue la persona elegida.`) 
     resultado.innerHTML = `Amigo sorteado: ${amigos[posicionAmigoDeLaLista]}`;
+    amigoElement.value = ''; // Se limpia el input
 }
