438f2812465d4f83a3dee1c7342bf8f98771b1fb
MisterBartra <jedubartra21@gmail.com>
2025-02-13 17:54:33 -0500

    Completando Desafío Secreto

--- app.js
+++ app.js
@@ -13,23 +13,24 @@ function encontrarEnAmigos(mensaje="", idLista = 0) {
 }
 
 function agregarAmigo() {
-    if (amigoElement.value !== '' && !encontrarEnAmigos("Existe un amigo con este nombre.")) { // Se descarta el vacío
+    if (amigoElement.value !== '' && !encontrarEnAmigos("Existe un amigo con este nombre.")) { // Se descarta el vacio y el nombre repetido
         amigoLIActual = document.createElement('li'); // Se prepara el elemento
         amigos.push(amigoElement.value); // Enlisto el valor dentro de amigos
         amigoLIActual.innerHTML = amigoElement.value; // Momento dónde se entrega el dato al nuevo elemento <li>
         amigosElement.appendChild(amigoLIActual); // Se agrega el elemento <li> en el elemento <ul>
-        amigoElement.value = ''; // Se limpia el input
     } else {
         alert(`Por favor, ingrese ${amigos[0] == undefined ? "un" : "otro"} nombre de amigo.`);
     }
+    amigoElement.value = ''; // Se limpia el input
 }
 
 function sortearAmigo() {
     if (amigos.length === 0) return alert("La lista de amigos está vacío.");
-    let posicionAmigoDeLaLista = Math.floor(Math.random()*amigos.length);
-	amigoEncontradoEnLista = encontrarEnAmigos("Exacto!", posicionAmigoDeLaLista);
-    if (!amigoEncontradoEnLista) alert(`La persona elegida fue ${amigos[posicionAmigoDeLaLista]}`)
-	else alert("Adivinaste!\n" + `${amigoElement.value} fue la persona elegida.`) 
+    else if (amigoElement.value === '') return alert("El campo a evaluar se encuentra vacío");
+    let posicionAmigoDeLaLista = Math.floor(Math.random() * amigos.length);
+	amigoEncontradoEnLista = amigoElement.value !== amigos[posicionAmigoDeLaLista] ? false : true; // Precisión de adivinar el amigo.
+    if (amigoEncontradoEnLista) alert("Adivinaste!"); else alert("Intente otra vez!");
+    alert(`La persona elegida fue\n  ${amigos[posicionAmigoDeLaLista]}`)
     resultado.innerHTML = `Amigo sorteado: ${amigos[posicionAmigoDeLaLista]}`;
     amigoElement.value = ''; // Se limpia el input
 }
